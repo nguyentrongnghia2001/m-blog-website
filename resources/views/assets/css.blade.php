940cb40e8c6f6aa665d0b94c72f24d95f3cc7599
nguyentrongnghia2001 <nghianhodoi@gmail.com>
2024-10-11 00:29:56 +0700

    init source laravel 11

--- resources/views/assets/css.blade.php
+++
@@ -1,3 +0,0 @@
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-crossorigin="anonymous" />
-<link href="{{ asset('css/app.css') }}" rel="stylesheet" />
