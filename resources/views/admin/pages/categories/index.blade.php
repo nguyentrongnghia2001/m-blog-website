4f29edf87734f39cd977bac46ed241bd268538fa
nguyentrongnghia2001 <nghianhodoi@gmail.com>
2024-10-30 22:55:26 +0700

    feat: get slug function

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -74,7 +74,7 @@
                         </div>
                         <div class="form-group">
                             <label for="slug">Slug</label>
-                            <input type="text" class="form-control" id="slug" name="slug" required>
+                            <input type="text" class="form-control" id="slug" name="slug" readonly="readonly" required>
                         </div>
                         <button type="submit" class="btn btn-primary">Save changes</button>
                     </form>
@@ -175,6 +175,44 @@
                     });
                 }
             });
+
+            // Handel get slug
+            $(document).on('keyup', '#name', function() {
+                var takedata = $('#name').val()         
+                $('#slug').val(toSlug(takedata));   
+            });
+
+            function toSlug(str) {
+                // Chuyển hết sang chữ thường
+                str = str.toLowerCase();     
+            
+                // xóa dấu
+                str = str.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
+                str = str.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
+                str = str.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
+                str = str.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
+                str = str.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
+                str = str.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
+                str = str.replace(/(đ)/g, 'd');
+            
+                // Xóa ký tự đặc biệt
+                str = str.replace(/([^0-9a-z-\s])/g, '');
+            
+                // Xóa khoảng trắng thay bằng ký tự -
+                str = str.replace(/(\s+)/g, '-');
+                
+                // Xóa ký tự - liên tiếp
+                str = str.replace(/-+/g, '-');
+            
+                // xóa phần dự - ở đầu
+                str = str.replace(/^-+/g, '');
+            
+                // xóa phần dư - ở cuối
+                str = str.replace(/-+$/g, '');
+            
+                // return
+                return str;
+            }
         });
     </script>
 @stop
